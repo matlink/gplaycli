98344bf0bfddeb6a529bb508675e12b2881b9e72
Matlink <matlink@matlink.fr>
2015-07-14 21:17:24 +0200

    [upd] supports bulk details fetch and support search not only free applications

--- gplaycli.py
+++ gplaycli.py
@@ -60,6 +60,23 @@ class GPlaycli(object):
 			apks_with_details.append([apk,apk_version_code])
 		return apks_with_details
 
+	def get_bulk_details(self, list_of_apks):
+		results = self.playstore_api.bulkDetails(list_of_apks)
+		details = dict()
+		for pos,apk in enumerate(list_of_apks):
+			det = results.entry[pos]
+			doc = det.doc
+			details[apk] = [ doc.title,\
+							doc.creator,\
+							self.sizeof_fmt(doc.details.appDetails.installationSize),\
+							doc.details.appDetails.numDownloads,\
+							doc.details.appDetails.uploadDate,\
+							doc.docid,\
+							str(doc.details.appDetails.versionCode),\
+							"%.2f" % doc.aggregateRating.starRating\
+							]
+		return details
+
 	def prepare_analyse_apks(self):
 	    download_folder_path = self.config["download_folder_path"]
 	    list_of_apks = [filename for filename in os.listdir(download_folder_path) if os.path.splitext(filename)[1] == ".apk"]
@@ -197,7 +214,7 @@ class GPlaycli(object):
 		#Query results
 		return self.playstore_api.search(search_string, nb_results=nb_results).doc
 
-	def search(self, results_list, search_string, nb_results):
+	def search(self, results_list, search_string, nb_results, free_only=True, include_headers=True):
 		results = self.raw_search(results_list, search_string, nb_results)
 		if len(results) > 0:
 			results = results[0].child
@@ -205,22 +222,24 @@ class GPlaycli(object):
 			print "No result"
 			return
 		all_results = list()
-		# Name of the columns
-		col_names = ["Title","Creator","Size","Downloads","Last Update","AppID","Version","Rating"]
-		all_results.append(col_names)
+		if include_headers:
+			# Name of the columns
+			col_names = ["Title","Creator","Size","Downloads","Last Update","AppID","Version","Rating"]
+			all_results.append(col_names)
 		# Compute results values
 		for result in results:
-			if result.offer[0].checkoutFlowRequired == False: #if Free to download
-				l = [ result.title,
-				result.creator,
-				self.sizeof_fmt(result.details.appDetails.installationSize),
-				result.details.appDetails.numDownloads,
-				result.details.appDetails.uploadDate,
-				result.docid,
-				result.details.appDetails.versionCode,
-				"%.2f" % result.aggregateRating.starRating
-				]
-				all_results.append(l)
+			if free_only and result.offer[0].checkoutFlowRequired == True: #if not Free to download
+				continue
+			l = [ result.title,
+			result.creator,
+			self.sizeof_fmt(result.details.appDetails.installationSize),
+			result.details.appDetails.numDownloads,
+			result.details.appDetails.uploadDate,
+			result.docid,
+			result.details.appDetails.versionCode,
+			"%.2f" % result.aggregateRating.starRating
+			]
+			all_results.append(l)
 
 		if self.verbose:
 			# Print a nice table
