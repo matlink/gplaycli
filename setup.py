3125041e98a227c914cb50ebb44e8a2b3c76fc71
Matlink <matlink@matlink.fr>
2015-08-24 19:25:45 +0200

    [upd] adding setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='GPlayCli',
+        version='0.1',
+        description='GPlayCli, a Google play downloader command line interface',
+        author="Matlink",
+        author_email="matlink@matlink.fr",
+        url="https://github.com/matlink/gplaycli",
+        packages=['gplaycli'],
+    )
