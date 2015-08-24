95531a94fa9d376f018e97c893e849a85719a114
matlink <matlink@matlink.fr>
2015-08-24 21:20:52 +0200

    [fix] setup install issue

--- setup.py
+++ setup.py
@@ -1,9 +1,5 @@
 from setuptools import setup, find_packages
-import shutil, os
-
-if not os.path.exists('bin'):
-    os.makedirs('bin')
-shutil.copyfile('gplaycli.py', 'bin/gplaycli')
+import os
 
 setup(name='GPlayCli',
         version='0.1',
@@ -12,14 +8,8 @@ setup(name='GPlayCli',
         author_email="matlink@matlink.fr",
         url="https://github.com/matlink/gplaycli",
         license="AGPLv3",
-        scripts=['bin/gplaycli'],
-        packages=[
-                'ext_libs/androguard',
-                'ext_libs/androguard/core',
-                'ext_libs/androguard/core/bytecodes',
-                'ext_libs/androguard/core/bytecodes/libdvm',
-                'ext_libs/googleplay_api'
-        ],
+        scripts=['gplaycli'],
+        packages=find_packages(), 
         install_requires=[
                 'requests',
                 'protobuf',
