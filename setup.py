e72a84cde1102aeee2f51fddf202cdc9197b1431
Matlink <matlink@matlink.fr>
2015-08-25 23:11:14 +0200

    [upd] adding debian packaging files, use make deb

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup, Command
 import os
 
 setup(name='GPlayCli',
@@ -9,11 +9,22 @@ setup(name='GPlayCli',
         url="https://github.com/matlink/gplaycli",
         license="AGPLv3",
         scripts=['gplaycli'],
-        packages=find_packages(), 
+        packages=[
+            'ext_libs/androguard/core/bytecodes/libdvm/',    
+            'ext_libs/androguard/core/bytecodes/',    
+            'ext_libs/androguard/core/',    
+            'ext_libs/androguard/',    
+            'ext_libs/googleplay_api/',    
+            'ext_libs/',    
+        ], 
+        data_files=[
+            ['/etc/gplaycli/', ['credentials.conf']],
+        ],
         install_requires=[
                 'requests',
                 'protobuf',
                 'ndg-httpsclient',
                 'clint',
+		'pyasn1',
         ],
     )
