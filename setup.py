12cc634eaad6ff21261345d616dc6b38b46da5db
Matlink <matlink@matlink.fr>
2016-12-20 10:34:30 +0100

    To version 0.2.1, recommends dummydroid for AndroidID generation

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, Command
 import os
 
 setup(name='GPlayCli',
-        version='0.1.5-2',
+        version='0.2.1',
         description='GPlayCli, a Google play downloader command line interface',
         author="Matlink",
         author_email="matlink@matlink.fr",
