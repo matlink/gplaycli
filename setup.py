ca72342a89fb6477b9697016aaa736e43e9c3682
Matlink <matlink@matlink.fr>
2017-03-26 12:06:46 +0200

    New version 0.2.2: adding token authentification

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, Command
 import os
 
 setup(name='GPlayCli',
-        version='0.2.1',
+        version='0.2.2',
         description='GPlayCli, a Google play downloader command line interface',
         author="Matlink",
         author_email="matlink@matlink.fr",
