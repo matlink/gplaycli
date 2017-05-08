d478af5ba4418aa22d689ebaa3349bc990ebc13f
Matlink <matlink@matlink.fr>
2017-05-08 22:30:57 +0200

    pytest for download

--- tests/test_init.py
+++ tests/test_init.py
@@ -3,9 +3,22 @@ import os
 
 sys.path.insert(0, os.path.abspath('.'))
 
+from gplaycli import gplaycli
+gpc = gplaycli.GPlaycli()
+
+token_url = "https://matlink.fr/token/email/gplaycliacc@gmail.com"
+
 def test_default_settings():
-    from gplaycli import gplaycli
-    gpc = gplaycli.GPlaycli()
     assert gpc.yes == False
     assert gpc.verbose == False
     assert gpc.progress_bar == False
+
+def test_connection():
+    gpc.token = True
+    gpc.retrieve_token(token_url)
+    success, error = gpc.connect_to_googleplay_api()
+    assert error is None
+    assert success == True
+
+def test_download_firefox():
+    gpc.download_packages(['org.mozilla.firefox'])
