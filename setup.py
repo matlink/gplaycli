80d4b864fb8ec7d8168e4b9d033aa32ead3702c1
Matlink <matlink@matlink.fr>
2017-05-14 12:26:25 +0200

    Add ability to write apps download status to logfiles with -L

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, Command
 import os
 
 setup(name='GPlayCli',
-        version='0.2.4',
+        version='0.2.5',
         description='GPlayCli, a Google play downloader command line interface',
         author="Matlink",
         author_email="matlink@matlink.fr",
