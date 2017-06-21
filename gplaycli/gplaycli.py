aa6d41f0a09ed53ccd49af603e8691eda57bad6f
Matlink <matlink@matlink.fr>
2017-06-22 00:06:33 +0200

    Bump to version 0.2.6

--- gplaycli/gplaycli.py
+++ gplaycli/gplaycli.py
@@ -45,6 +45,7 @@ except DistributionNotFound:
 class ERRORS(IntEnum):
     OK = 0
     TOKEN_DISPENSER_AUTH_ERROR = 5
+    TOKEN_DISPENSER_SERVER_ERROR = 6
     KEYRING_NOT_INSTALLED = 10
     CANNOT_LOGIN_GPLAY = 15
 
@@ -110,6 +111,9 @@ class GPlaycli(object):
         if token == 'Auth error':
             print 'Token dispenser auth error, probably too many connections'
             sys.exit(ERRORS.TOKEN_DISPENSER_AUTH_ERROR)
+        elif token == "Server error":
+            print 'Token dispenser server error'
+            sys.exit(ERRORS.TOKEN_DISPENSER_SERVER_ERROR)
         return token
 
     def set_download_folder(self, folder):
