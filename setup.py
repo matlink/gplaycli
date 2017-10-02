b78a26fbcb425ee619bf2f7baf66948908f643bc
Matlink <matlink@matlink.fr>
2017-10-02 21:23:00 +0200

    Bump to version 2.16
    Signed-off-by: Matlink <matlink@matlink.fr>

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ else:
 
 
 setup(name='GPlayCli',
-        version='2.15',
+        version='2.16',
         description='GPlayCli, a Google play downloader command line interface',
         author="Matlink",
         author_email="matlink@matlink.fr",
