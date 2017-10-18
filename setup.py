88146407bc7bda4a27767212fbb69bc50b102111
Matlink <matlink@matlink.fr>
2017-10-18 23:09:10 +0200

    Version 3.6
    Signed-off-by: Matlink <matlink@matlink.fr>

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ else:
 
 
 setup(name='GPlayCli',
-        version='3.5',
+        version='3.6',
         description='GPlayCli, a Google play downloader command line interface',
         author="Matlink",
         author_email="matlink@matlink.fr",
