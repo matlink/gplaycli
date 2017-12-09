09cbc54ccdf10a7355b99be0a83613b88dbc00f2
Matlink <matlink@matlink.fr>
2017-12-09 17:36:23 +0100

    moved load_from_file

--- gplaycli/util.py
+++ gplaycli/util.py
@@ -4,3 +4,5 @@ def sizeof_fmt(num):
     log = int(math.log(num, 1024))
     return "%.2f%s" % (num/(1024**log), ['bytes','KB','MB','GB','TB'][log])
 
+def load_from_file(filename):
+    return [package.strip('\r\n') for package in open(filename).readlines()]
