0a6af07efb4b36e51ace4a967e2a08306bf04057
Matlink <matlink@matlink.fr>
2017-12-26 17:02:24 +0100

    moved method to function in util.py

--- gplaycli/util.py
+++ gplaycli/util.py
@@ -1,3 +1,4 @@
+import os
 import math
 
 def sizeof_fmt(num):
@@ -6,3 +7,10 @@ def sizeof_fmt(num):
 
 def load_from_file(filename):
     return [package.strip('\r\n') for package in open(filename).readlines()]
+
+def list_folder_apks(folder):
+    """
+    List apks in the given folder
+    """
+    list_of_apks = [filename for filename in os.listdir(folder) if filename.endswith(".apk")]
+    return list_of_apks
