ed28c1feca591ceeba5609b8ebdbd771ae05d44e
Matlink <matlink@matlink.fr>
2018-03-08 14:33:51 +0100

    Version 3.21

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ if sys.version_info[0] == 2:
     sys.exit(1)
 
 setup(name='GPlayCli',
-        version='3.20',
+        version='3.21',
         description='GPlayCli, a Google play downloader command line interface',
         author="Matlink",
         author_email="matlink@matlink.fr",
