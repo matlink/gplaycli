2a1d9a60719e069dede8e0d9150046ca2ca98557
Matlink <matlink@matlink.fr>
2018-03-25 10:36:31 +0530

    Fixes #171

--- gplaycli/gplaycli.py
+++ gplaycli/gplaycli.py
@@ -338,7 +338,7 @@ class GPlaycli:
 				continue
 			detail = [result['title'],
 					  result['author'],
-					  util.sizeof_fmt(result['installationSize']),
+					  util.sizeof_fmt(result['installationSize']) if result['installationSize'] > 0 else 'N/A',
 					  result['numDownloads'],
 					  result['uploadDate'],
 					  result['docId'],
