7d9885034d468af185ca1948f039f151a343e07b
Matlink <matlink@matlink.fr>
2018-05-15 13:30:25 +0200

    cleaner progress bar

--- gplaycli/util.py
+++ gplaycli/util.py
@@ -5,8 +5,10 @@ import time
 
 
 def sizeof_fmt(num):
+	if not num:
+		return "00.00KB"
 	log = int(math.log(num, 1024))
-	return "%.2f%s" % (num/(1024**log), ['bytes','KB','MB','GB','TB'][log])
+	return "%.2f%s" % (num/(1024**log), ['B ','KB','MB','GB','TB'][log])
 
 def load_from_file(filename):
 	return [package.strip('\r\n') for package in open(filename).readlines()]
@@ -30,36 +32,17 @@ def vcode(string_vcode):
 
 ### progress bar ###
 """ copyright https://github.com/kennethreitz/clint """
-STREAM = sys.stderr
-
-BAR_TEMPLATE = '%s[%s%s] %i/%i - %s\r'
-
-BAR_FILLED_CHAR = '#'
-BAR_EMPTY_CHAR = ' '
-
-# How long to wait before recalculating the ETA
-ETA_INTERVAL = 1
-# How many intervals (excluding the current one) to calculate the simple moving
-# average
-ETA_SMA_WINDOW = 9
-
 class progressbar(object):
-	def __enter__(self):
-		return self
-
-	def __exit__(self, exc_type, exc_val, exc_tb):
-		self.done()
-		return False  # we're not suppressing exceptions
-
-	def __init__(self, label='', width=32, hide=None, empty_char=BAR_EMPTY_CHAR,
-				 filled_char=BAR_FILLED_CHAR, expected_size=None, every=1):
+	def __init__(self, label='', width=32, hide=None, empty_char=' ',
+				 filled_char='#', expected_size=None, every=1, eta_interval=1,
+				 eta_sma_window=9, stream=sys.stderr, bar_template='%s[%s%s] %s/%s - %s %s/s\r'):
 		self.label = label
 		self.width = width
 		self.hide = hide
 		# Only show bar in terminals by default (better for piping, logging etc.)
 		if hide is None:
 			try:
-				self.hide = not STREAM.isatty()
+				self.hide = not stream.isatty()
 			except AttributeError:  # output does not support isatty()
 				self.hide = True
 		self.empty_char = empty_char
@@ -72,6 +55,11 @@ class progressbar(object):
 		self.etadelta = time.time()
 		self.etadisp = self.format_time(self.eta)
 		self.last_progress = 0
+		self.eta_interval = eta_interval
+		self.eta_sma_window = eta_sma_window
+		self.stream = stream
+		self.bar_template = bar_template
+		self.speed = 0
 		if (self.expected_size):
 			self.show(0)
 
@@ -81,36 +69,35 @@ class progressbar(object):
 		if self.expected_size is None:
 			raise Exception("expected_size not initialized")
 		self.last_progress = progress
-		if (time.time() - self.etadelta) > ETA_INTERVAL:
+		if (time.time() - self.etadelta) > self.eta_interval:
 			self.etadelta = time.time()
-			self.ittimes = \
-				self.ittimes[-ETA_SMA_WINDOW:] + \
-					[-(self.start - time.time()) / (progress+1)]
-			self.eta = \
-				sum(self.ittimes) / float(len(self.ittimes)) * \
-				(self.expected_size - progress)
+			self.ittimes = (self.ittimes[-self.eta_sma_window:]
+							+ [-(self.start - time.time()) / (progress+1)])
+			self.eta = (sum(self.ittimes) / float(len(self.ittimes))
+						* (self.expected_size - progress))
 			self.etadisp = self.format_time(self.eta)
+			self.speed = 1 / (sum(self.ittimes) / float(len(self.ittimes)))
 		x = int(self.width * progress / self.expected_size)
 		if not self.hide:
 			if ((progress % self.every) == 0 or	  # True every "every" updates
 				(progress == self.expected_size)):   # And when we're done
-				STREAM.write(BAR_TEMPLATE % (
+				self.stream.write(self.bar_template % (
 					self.label, self.filled_char * x,
-					self.empty_char * (self.width - x), progress,
-					self.expected_size, self.etadisp))
-				STREAM.flush()
+					self.empty_char * (self.width - x), sizeof_fmt(progress),
+					sizeof_fmt(self.expected_size), self.etadisp, sizeof_fmt(self.speed)))
+				self.stream.flush()
 
 	def done(self):
 		self.elapsed = time.time() - self.start
 		elapsed_disp = self.format_time(self.elapsed)
 		if not self.hide:
 			# Print completed bar with elapsed time
-			STREAM.write(BAR_TEMPLATE % (
+			self.stream.write(self.bar_template % (
 				self.label, self.filled_char * self.width,
-				self.empty_char * 0, self.last_progress,
-				self.expected_size, elapsed_disp))
-			STREAM.write('\n')
-			STREAM.flush()
+				self.empty_char * 0, sizeof_fmt(self.last_progress),
+				sizeof_fmt(self.expected_size), elapsed_disp, sizeof_fmt(self.speed)))
+			self.stream.write('\n')
+			self.stream.flush()
 
 	def format_time(self, seconds):
 		return time.strftime('%H:%M:%S', time.gmtime(seconds))
