78b1db8349037706c04f73ab84ca817b1097fa4e
Matlink <matlink@matlink.fr>
2020-02-24 14:18:08 +0100

    Add long description for pypi

--- setup.py
+++ setup.py
@@ -2,9 +2,14 @@ from setuptools import setup
 import os
 import sys
 
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+
 setup(name='gplaycli',
 		version='3.28',
 		description='GPlayCli, a Google play downloader command line interface',
+		long_description=long_description,
+		long_description_content_type="text/markdown",
 		author="Matlink",
 		author_email="matlink@matlink.fr",
 		url="https://github.com/matlink/gplaycli",
