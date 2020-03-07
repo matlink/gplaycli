4af7d3d6a991de4d4c69bbf09d26ea6554f880d1
Matlink <matlink@matlink.fr>
2020-03-07 13:31:01 +0100

    Version 3.29

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 	long_description = fh.read()
 
 setup(name='gplaycli',
-		version='3.28',
+		version='3.29',
 		description='GPlayCli, a Google play downloader command line interface',
 		long_description=long_description,
 		long_description_content_type="text/markdown",
@@ -29,7 +29,7 @@ setup(name='gplaycli',
 			['etc/gplaycli', ['gplaycli.conf']],
 		],
 		install_requires=[
-				'matlink-gpapi>=0.4.4.3',
+				'matlink-gpapi>=0.4.4.4',
 				'pyaxmlparser',
 		],
 )
